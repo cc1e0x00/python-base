3fa06635ed8880e6d6a07c0cc1e6c957b9d13d18
cc1e0x00 <01ralves@gmail.com>
2022-04-27 21:41:24 -0400

    Implentação Hello word multi linguas

--- hello.py
+++ hello.py
@@ -1,4 +1,42 @@
 #!/usr/bin/env python3
 
-# Este programa imprime hello world
-print("hello, World!")
+""" "Hello world" multi-languages.
+
+Based on languages configured in ENV "LANG".
+
+How to use:
+
+Set your variable LANG in a correct way.
+
+Example:
+    
+    export LANG=en_US.UTF-8
+    export LANG=pt_BR
+
+Run
+    python3 hell0.py
+    ou
+    ./hello.py
+
+"""
+__version__  = "0.0.1"
+__author__ = "cc1e0x00"
+__license__ = "Unlicense"
+
+
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+msg = "Hello, Word!"
+
+
+if  current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+elif current_language == "es_SP":
+    msg = "Hola, Mundo!"
+elif current_language == "fr_FR":
+    msg - "Bonjour Monde!"
+
+print(msg)
